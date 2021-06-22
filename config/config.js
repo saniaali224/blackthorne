522ee0bf7cc8dbc4f9210acc6cf903bf127cf313
saniaali224 <saniaalikhan224@gmail.com>
2021-06-22 17:43:44 +0500

    done

--- config/config.js
+++ config/config.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-const { DB_HOST, DB_USERNAME, DB_PASSWORD } = process.env;
+const { DB_HOST, DB_USERNAME, DB_PASSWORD , DB_DATABASE} = process.env;
 module.exports={
   "development": {
     "username": DB_USERNAME,
